a5ff4af8b470d02e5234f0370dea309e78580922
Nano.WANG <namiheike@gmail.com>
2016-03-15 21:45:32 +0800

    deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,15 @@
 # TODO update secrets
 git pull
 
+# update deps
+bundle
+
 # restart goliath
+# TODO try god
+# TODO
 
-
-# restart sidekiq
+# stop sidekiq
 sidekiqctl stop ./tmp/pids/sidekiq.pid
+
+# start sidekiq
+bundle exec sidekiq -C ./config/sidekiq.yml -v -r ./build_server.rb -g aurora-core-build-server -d
--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,7 @@
 # rvm
 
 # start goliath
-bundle exec ruby ./build_server.rb -p 4000 -e production -d -P ./tmp/pids/goliath.pid -l ./log/goliath.log -S ./a.socket
+bundle exec ruby ./build_server.rb -p 4000 -e production -d -P ./tmp/pids/goliath.pid -l ./log/goliath.log -S ./tmp/sockets/goliath.sock
 
 # start sidekiq
 bundle exec sidekiq -C ./config/sidekiq.yml -v -r ./build_server.rb -g aurora-core-build-server -d
