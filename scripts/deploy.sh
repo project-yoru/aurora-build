a88dac114125ef85d951a2bfc3a5e5374235de45
Nano.WANG <namiheike@gmail.com>
2016-03-29 15:04:41 +0800

    deployment scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# update code
+# TODO update secrets
+git pull
+git submodule update --remote
+
+# update deps
+bundle
+
+# stop goliath
+kill -9 `cat ./tmp/pids/goliath.pid`
+
+# stop sidekiq
+sidekiqctl stop ./tmp/pids/sidekiq.pid
+
+# start server
+source ./start_server.sh
