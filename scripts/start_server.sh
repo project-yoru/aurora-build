888a9fcda18354c1db38a161b5bddc1d4f201d44
Nano.WANG <namiheike@gmail.com>
2016-04-16 13:52:01 +0800

    update label for sidekiq

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,4 +6,4 @@
 bundle exec ruby ./build_server.rb -v -e production -P ./tmp/pids/goliath.pid -l ./log/goliath.log -S ./tmp/sockets/goliath.sock -d
 
 # start sidekiq
-bundle exec sidekiq -C ./config/sidekiq.yml -v -r ./build_server.rb -g aurora-core-build-server -e production -d
+bundle exec sidekiq -C ./config/sidekiq.yml -v -r ./build_server.rb -g aurora-build -e production -d
